54dd8f84ab02490451a78cf98e92089849a3e579
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-10-29 23:57:06 +0530

    update user and todo schema with validation in mongoose

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,13 +1,46 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  status: String,
-  dueDate: Date,
-  priority: String,
-  createdAt: Date,
-  updatedAt: Date
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "User ID is required"]
+  },
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    minLength: [5, "Title must be at least 3 character long"],
+    maxLength: [150, "Title can not exceed 150 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Title is required"],
+    minLength: [5, "Descripton must be at least 3 character long"],
+    maxLength: [250, "Description can not exceed 250 characters"],
+  },
+  status: {
+    type: String,
+    enum: ["pending", "in-progress", "complete"],
+    default: "pending"
+  },
+  dueDate: {
+    type: Date,
+
+  },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high", "urgent"],
+    default: "medium"
+
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 export default mongoose.model("Todo", todoSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  createdAt: Date,
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    minLength: [4, "Username must be at least 4 character long."],
+    maxLength: [20, "Username can not exceed 20 characters."]
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/, "Please enter a valid email address"],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters"],
+  },
+  createdAt: {
+    type: Date,
+    deafult: Date.now
+  }
 });
 
 export default mongoose.model("User", userSchema);
