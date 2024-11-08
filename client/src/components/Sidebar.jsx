1d93ecd96eb34028596803ba74102b06ced8fcd4
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-11-08 16:50:39 +0530

    add animation to sidebar nav

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,73 +8,171 @@ import { FaListCheck } from "react-icons/fa6";
 import { FaNoteSticky } from "react-icons/fa6";
 import { FaCalendarDays } from "react-icons/fa6";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { useState } from "react";
+
+import { motion } from "framer-motion";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [selected, setSelected] = useState(null);
+
+  const style = {
+    width: isOpen ? "fit-content" : "50px",
+    height: isOpen ? "100%" : "fit-content",
+    // padding: isOpen ? "1.5rem" : "1rem",
+  };
+
   return (
-    <div className={styles.sidebar}>
-      <div className={styles.navHeadingBox}>
-        <h2 className={styles.navHeading}>Menu</h2>
-        <FaBars size="2.4rem" className={styles.menuBars} />
-      </div>
-      <div className={styles.sidebarItems}>
-        <div className={styles.searchBox}>
-          <FaMagnifyingGlass />
-          <input
-            type="text"
-            placeholder="Search"
-            className={styles.searchInput}
+    <motion.nav
+      layout
+      // animate={{
+      //   transition: {
+      //     delay: 0.3,
+      //   },
+      // }}
+      className={styles.sidebar}
+      style={style}
+    >
+      <motion.div layout className={styles.navHeadingBox}>
+        {isOpen && <h2 className={styles.navHeading}>Menu</h2>}
+        <div>
+          <motion.div layout>
+            <FaBars
+              size="2.4rem"
+              className={styles.menuBars}
+              onClick={() => setIsOpen(isOpen => !isOpen)}
+            />
+          </motion.div>
+        </div>
+      </motion.div>
+
+      {isOpen && <SidebarSearch />}
+
+      {isOpen && (
+        <div className={styles.sidebarItems}>
+          <h3>Tasks</h3>
+
+          <SidebarItems
+            icon={<FaAnglesRight />}
+            title="Today"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
+
+          <SidebarItems
+            icon={<FaListCheck />}
+            title="Tomorrow"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
+
+          <SidebarItems
+            icon={<FaCalendarDays />}
+            title="Calender"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
+          <SidebarItems
+            icon={<FaNoteSticky />}
+            title="Sticky Wall"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
           />
         </div>
-      </div>
-      <div className={styles.sidebarItems}>
-        <h3>Tasks</h3>
+      )}
 
-        <div className={styles.sidebarNavs}>
-          <FaAnglesRight />
-          <span>Upcoming</span>
-        </div>
-        <div className={styles.sidebarNavs}>
-          <FaListCheck />
-          <span>Today</span>
-        </div>
-        <div className={styles.sidebarNavs}>
-          <FaCalendarDays />
-          <span>Calendar</span>
-        </div>
-        <div className={styles.sidebarNavs}>
-          <FaNoteSticky />
-          <span>Sticky Wall</span>
-        </div>
-      </div>
-      <div className={styles.sidebarItems}>
-        <h3>Lists</h3>
-        <div className={styles.sidebarNavs}>
-          <div className={styles.listBox}></div>
-          <span>Personal</span>
-        </div>
-        <div className={styles.sidebarNavs}>
-          <div className={styles.listBox}></div>
-          <sxpan>Work</sxpan>
-        </div>
-        <div className={styles.sidebarNavs}>
-          <div className={styles.listBox}></div>
+      {isOpen && (
+        <div className={styles.sidebarItems}>
+          <h3>Lists</h3>
 
-          <span>List1</span>
+          <SidebarItems
+            color={"red"}
+            title="Personal"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
+          <SidebarItems
+            color={"red"}
+            title="Work"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
+          <SidebarItems
+            color={"red"}
+            title="Study"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
         </div>
-      </div>
-      <div className={styles.sidebarItems}>
-        <h3>Tags</h3>
-      </div>
+      )}
 
-      <div className={`${styles.sidebarItems} ${styles.last}`}>
-        <div className={styles.sidebarNavs}>
-          <FaSliders />
-          <span>Settings</span>
+      {isOpen && (
+        <div className={styles.sidebarItems}>
+          <h3>Tags</h3>
         </div>
-        <div className={styles.sidebarNavs}>
-          <FaSignOutAlt />
-          <span>Sign out</span>
+      )}
+
+      {isOpen && (
+        <div className={`${styles.sidebarItems} ${styles.last}`}>
+          <SidebarItems
+            icon={<FaSliders />}
+            title="Settings"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
+          <SidebarItems
+            icon={<FaSignOutAlt />}
+            title="Sign out"
+            selected={selected}
+            setSelected={setSelected}
+            isOpen={isOpen}
+          />
         </div>
+      )}
+    </motion.nav>
+  );
+}
+
+function SidebarItems({ icon, title, color, selected, setSelected, isOpen }) {
+  const style = {
+    // width: isOpen ? "fit-content" : "60x",
+    backgroundColor: selected === title ? "#bcbcbc" : "",
+  };
+  return (
+    <div
+      className={styles.sidebarNavs}
+      style={style}
+      onClick={() => setSelected(title)}
+    >
+      {icon || (
+        <div
+          className={styles.listBox}
+          style={{ backgroundColor: color }}
+        ></div>
+      )}
+      {isOpen && <span>{title}</span>}
+    </div>
+  );
+}
+
+function SidebarSearch() {
+  return (
+    <div className={styles.sidebarItems}>
+      <div className={styles.searchBox}>
+        <FaMagnifyingGlass />
+        <input
+          type="text"
+          placeholder="Search"
+          className={styles.searchInput}
+        />
       </div>
     </div>
   );
